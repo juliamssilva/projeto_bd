20ffe5077418f8a93f2455e1b7dfacbe28b33273
Humberto nunes <nuneshumberto120@gmail.com>
2025-04-05 20:18:02 -0300

    Troca do banco para SQL SERVER e configuração do servidor

--- ProjCrud/conexao.cs
+++ ProjCrud/conexao.cs
@@ -4,7 +4,7 @@ namespace ProjCrud
 {
     public static class Conexao
     {
-        private static string conexaoString = "Server=localhost;Database=MeuBanco;User Id=sa;Password=MinhaSenha;";
+        private static string conexaoString = "Server=192.168.0.3;Database=Livraria;User Id=sa;Password=Teixeira;";
 
         public static SqlConnection Conectar()
         {
